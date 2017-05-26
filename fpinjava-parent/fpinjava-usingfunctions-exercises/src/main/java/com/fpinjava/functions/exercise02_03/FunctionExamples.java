a42f5509076306b460a4f5a31704ca3117526dc7
Even Holthe <even.holthe@me.com>
2017-05-26 11:18:45 +0200

    Using Functions: 02_01 through 02_05

--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_03/FunctionExamples.java
@@ -3,9 +3,9 @@ package com.fpinjava.functions.exercise02_03;
 
 public class FunctionExamples {
 
-  public static final Function<Integer, Function<Integer, Integer>> add = null; // To be implemented
+  public static final Function<Integer, Function<Integer, Integer>> add = first -> second -> first + second;
 
-  public static final BinaryOperator add2 =  null; // To be implemented
+  public static final BinaryOperator add2 = first -> second -> first + second;
 
-  public static final BinaryOperator mult =  null; // To be implemented
+  public static final BinaryOperator mult =  first -> second -> first * second;
 }
--- fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
+++ fpinjava-parent/fpinjava-usingfunctions-exercises/src/main/java/com/fpinjava/functions/exercise02_05/Function.java
@@ -4,7 +4,9 @@ public interface Function<T, U> {
 
   U apply(T arg);
 
-  static <T, U, V> Function<Function<U, V>, Function<Function<T, U>, Function<T, V>>> higherCompose() {
-    throw new RuntimeException("To be implemented.");
+  static <T, U, V> Function<Function<U, V>,
+                   Function<Function<T, U>,
+                            Function<T, V>>> higherCompose() {
+    return x -> y -> v -> x.apply(y.apply(v));
   }
 }
