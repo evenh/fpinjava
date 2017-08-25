abfd161d795451596987a01545fce34fcbca99ba
Even Holthe <even.holthe@me.com>
2017-08-25 14:45:57 +0200

    Recursion: 04_04

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_04/Range.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_04/Range.java
@@ -1,11 +1,21 @@
 package com.fpinjava.recursion.exercise04_04;
 
+import static com.fpinjava.common.CollectionUtilities.append;
+
+import com.fpinjava.common.TailCall;
+import java.util.Collections;
 import java.util.List;
 
 
 public class Range {
 
   public static List<Integer> range(Integer start, Integer end) {
-    throw new RuntimeException("To be implemented.");
+    return range_(Collections.emptyList(), start, end).eval();
+  }
+
+  public static TailCall<List<Integer>> range_(List<Integer> acc, Integer start, Integer end) {
+    return end <= start
+        ? TailCall.ret(acc)
+        : TailCall.sus(() -> range_(append(acc, start), start + 1, end));
   }
 }
