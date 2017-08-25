9327186f2eb898e8b3817db139ce17ab13421c17
Even Holthe <even.holthe@me.com>
2017-08-25 14:20:39 +0200

    Recursion: 04_01

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_01/Fibonacci.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_01/Fibonacci.java
@@ -1,10 +1,21 @@
 package com.fpinjava.recursion.exercise04_01;
 
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+
 import java.math.BigInteger;
 
 public class Fibonacci {
-  
+
   public static BigInteger fib(int x) {
-    throw new RuntimeException("To be implemented.");
+    return fib_(ONE, ZERO, BigInteger.valueOf(x));
+  }
+
+  private static BigInteger fib_(BigInteger acc1, BigInteger acc2, BigInteger x) {
+    return x.equals(ZERO)
+        ? ZERO
+        : x.equals(ONE)
+          ? acc1.add(acc2)
+          : fib_(acc2, acc1.add(acc2), x.subtract(ONE));
   }
 }
