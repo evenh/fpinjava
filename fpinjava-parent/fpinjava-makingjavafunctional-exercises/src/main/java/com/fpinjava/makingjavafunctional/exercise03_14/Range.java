37e241b5ab1e01f58a545ac861835881392c0478
Even Holthe <even.holthe@me.com>
2017-08-25 11:49:50 +0200

    Java more functional: 03_14

--- fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_14/Range.java
+++ fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_14/Range.java
@@ -1,11 +1,16 @@
 package com.fpinjava.makingjavafunctional.exercise03_14;
 
+import static com.fpinjava.makingjavafunctional.exercise03_13.CollectionUtilities.list;
+import static com.fpinjava.makingjavafunctional.exercise03_13.CollectionUtilities.prepend;
+
 import java.util.List;
 
 
 public class Range {
 
   public static List<Integer> range(Integer start, Integer end) {
-    throw new RuntimeException("To be implemented");
+    return end <= start
+        ? list()
+        : prepend(start, range(start + 1, end));
   }
 }
